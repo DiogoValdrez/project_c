69736bbd2e87ba887d0c22ecf478c8cee0dcfee3
Diogo Valdrez <diogovaldrez@tecnico.ulisboa.pt>
2021-03-29 19:05:00 +0100

    Added the basic board as a array

--- main.c
+++ main.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void board(int height, int width);
+
+void draw_board(char board[15][24], int height, int width);
 
 int main()
 {
     int height, width;
-    printf("Insert the board size in the following format: height, width: \n");
-    scanf(" %d %d", &height, &width);
-    board(height, width);
+    scanf("%d", &height);
+    scanf("%d", &width);
+    char board[15][24];
+    int i, j;
+    for ( i = 0; i < height; i++)
+    {
+        for ( j = 0; j < width; j++)
+        {
+            board[i][j] = '-';
+        }
+
+    }
+    draw_board(board, height, width);
     return EXIT_SUCCESS;
 }
 
-void board(int height, int width)
+
+
+void draw_board(char board[15][24],int height, int width)
 {
-    for(int i = 0; i < height; i++)
+    int i, j;
+    for (i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for ( j = 0; j < width; j++)
         {
-            printf("-");
-        }  
-        printf("\n");      
+            printf("%c", board[i][j]);
+        }
+        printf("\n");
     }
-    return;
+    return ;
 }
