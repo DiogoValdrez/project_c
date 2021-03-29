c08c09381ea9a957a49a4e8405d909c02cc17690
Diogo Valdrez <diogovaldrez@tecnico.ulisboa.pt>
2021-03-29 19:58:40 +0100

    Adeed basic board that as coordenates

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+void init_board(char board[15][24], int height, int width);
 void draw_board(char board[15][24], int height, int width);
 
 int main()
 {
     int height, width;
-    scanf("%d", &height);
-    scanf("%d", &width);
     char board[15][24];
+    printf("Imput the height and width of the board: \n");
+    scanf(" %d %d", &height, &width);
+    
+    init_board(board, height, width);
+    draw_board(board, height, width);
+    return EXIT_SUCCESS;
+}
+
+void init_board(char board[15][24], int height, int width)
+{
     int i, j;
     for ( i = 0; i < height; i++)
     {
@@ -19,22 +27,38 @@ int main()
         }
 
     }
-    draw_board(board, height, width);
-    return EXIT_SUCCESS;
 }
-
-
 
 void draw_board(char board[15][24],int height, int width)
 {
     int i, j;
+
+    printf("\n");
+    
     for (i = 0; i < height; i++)
     {
-        for ( j = 0; j < width; j++)
+        if((height - i) < 10)
         {
-            printf("%c", board[i][j]);
+            printf("%d ", (height - i));;
+        }else{
+            printf("%d", (height - i));;
+        }
+        
+        for (j = 0; j < width; j++)
+        {
+            printf("%c ", board[i][j]);
         }
         printf("\n");
     }
+
+    printf("  ");
+ 
+    for (i = 0; i < width; i++)
+    {
+        char num = i + 'A';
+        printf("%c ", num);
+    }
+    printf("\n");
+
     return ;
 }
