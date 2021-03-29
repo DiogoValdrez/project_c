027ea0e0c019375e9f46c451fdb66756caaa7638
Diogo Valdrez <diogovaldrez@tecnico.ulisboa.pt>
2021-03-29 20:13:12 +0100

    Added board with restrictions

--- main.c
+++ main.c
@@ -3,17 +3,35 @@
 
 void init_board(char board[15][24], int height, int width);
 void draw_board(char board[15][24], int height, int width);
+int mult(int value);
 
 int main()
 {
     int height, width;
     char board[15][24];
-    printf("Imput the height and width of the board: \n");
-    scanf(" %d %d", &height, &width);
+    do
+    {
+        printf("Imput the height and width of the board: \n");
+        printf("Both coordenates must be multiples of 3.\n");
+        printf("->The height must be between 9 and 15;\n");
+        printf("->The width must be between 9 and 24;\n");
+        scanf(" %d %d", &height, &width);
+    } while (height < 9 || height > 15 || width < 9 || width > 24 || mult(width) || mult(height)==1);
+    
     
     init_board(board, height, width);
     draw_board(board, height, width);
     return EXIT_SUCCESS;
+}
+
+int mult(int value)
+{
+    if(value%3 == 0)
+    {
+        return 0;
+    }else{
+        return 1;
+    }
 }
 
 void init_board(char board[15][24], int height, int width)
