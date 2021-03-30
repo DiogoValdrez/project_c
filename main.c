35b396027d2bb28ea113def6e35e702c87a85e1d
MEEC PROG 1920 <pedroraposo220@gmail.com>
2021-03-31 00:12:51 +0100

    Fixed restric_0_1 function

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ void init_board(char board[15][24], int height, int width);
 void draw_board(char board[15][24], int height, int width);
 int mult(int value);
 void pieces(char id_piece,int id_var,char board[15][24],int coor_height,int coor_width);
-int check_piece(char board[15][24], int coor_height, int coor_width);
+int restric_0_1(char board[15][24], int coor_height, int coor_width);
 
 int main()
 {
@@ -363,100 +363,77 @@ void pieces(char id_piece,int id_var,char board[15][24],int coor_height,int coor
 /*Function that checks whether the piece that was put in the board
 can be there considering the restrictions returning a 0 in case
  it's well positioned and any other number in case it shouldn't be there*/
- int check_piece(char board[15][24], int coor_height,int coor_width)
+ int restric_0_1(char board[15][24], int coor_height,int coor_width)
  {
-     int i, check_value = 0;
+     int i;
      if (!(coor_height == 0 || coor_width == 0) )
      {
-        if (board[coor_height][coor_width] != '-')// 0x0
+        if (board[coor_height][coor_width] != '-' &&
+            (board[coor_height+1][coor_width-1] != '-' || board[coor_height][coor_width-1] != '-'|| board[coor_height-1][coor_width-1] != '-' || board[coor_height-1][coor_width] != '-' ||board[coor_height-1][coor_width +1] != '-'))// 0x0
         {
-            if ( board[coor_height+1][coor_width-1] != '-' || board[coor_height][coor_width-1] != '-' || board[coor_height-1][coor_width-1] != '-' || board[coor_height-1][coor_width] != '-' ||board[coor_height-1][coor_width +1] != '-')
-                {
-                    check_value = 1;
-                }
+            return 1;
         }
-        if (board[coor_height+1][coor_width] != '-')// 1x0
+        if (board[coor_height+1][coor_width] != '-' &&
+            (board[coor_height+2][coor_width-1] != '-' || board[coor_height+1][coor_width-1] != '-' || board[coor_height][coor_width-1] != '-'))// 1x0
         {
-            if ( board[coor_height+2][coor_width-1] != '-' || board[coor_height+1][coor_width-1] != '-' || board[coor_height][coor_width-1] != '-')
-            {
-                check_value = 1;
-            }
+            return 1;
         }
-        if (board[coor_height+2][coor_width] != '-')// 2x0
+        if (board[coor_height+2][coor_width] != '-' &&
+            (board[coor_height+2][coor_width-1] != '-' || board[coor_height+1][coor_width-1] != '-'))// 2x0
         {
-            if ( board[coor_height+2][coor_width-1] != '-' || board[coor_height+1][coor_width-1] != '-')
-            {
-                check_value = 1;
-            }
+            return 1;
         }
-         if (board[coor_height][coor_width+1] != '-')// 0x1
+         if (board[coor_height][coor_width+1] != '-' &&
+              (board[coor_height-1][coor_width] != '-' || board[coor_height-1][coor_width+1] != '-' || board[coor_height-1][coor_width+2] != '-'))// 0x1
         {
-            if ( board[coor_height-1][coor_width] != '-' || board[coor_height-1][coor_width+1] != '-' || board[coor_height-1][coor_width+2] != '-')
-            {
-                check_value = 1;
-            }
+            return 1;
         }
-        if (board[coor_height][coor_width+2] != '-')// 0x2
+        if (board[coor_height][coor_width+2] != '-' &&
+            (board[coor_height-1][coor_width+1] != '-' || board[coor_height-1][coor_width+2] != '-'))// 0x2
         {
-            if ( board[coor_height-1][coor_width+1] != '-' || board[coor_height-1][coor_width+2] != '-')
-            {
-                check_value = 1;
-            }
+            return 1;
         }
-     }else
+        return 0;
+     }else if (coor_height == 0 && coor_width != 0)
      {
-         if (coor_height == 0 && coor_width != 0)
+            if (board[coor_height][coor_width] != '-' &&
+                (board[coor_height][coor_width-1] != '-' || board[coor_height+1][coor_width-1] != '-'))// 0x0
+            {
+                return 1;
+            }
+            if (board[coor_height+1][coor_width] != '-' &&
+                    (board[coor_height+2][coor_width-1] != '-' || board[coor_height+1][coor_width-1] != '-' || board[coor_height][coor_width-1] != '-'))// 1x0
+            {
+                         return 1;
+            }
+            if (board[coor_height+2][coor_width] != '-' &&
+                (board[coor_height+2][coor_width-1] != '-' || board[coor_height+1][coor_width-1] != '-'))// 2x0
+            {
+                         return 1;
+            }
+            return 0;
+         }else if (coor_height != 0 && coor_width == 0)
          {
-            if (board[coor_height][coor_width] != '-')// 0x0
+            if (board[coor_height][coor_width] != '-' &&
+                (board[coor_height-1][coor_width] != '-' || board[coor_height-1][coor_width+1] != '-'))// 0x0
             {
-                if ( board[coor_height][coor_width-1] != '-' || board[coor_height+1][coor_width-1] != '-')
-                {
-                    check_value = 1;
-                }
+                         return 1;
             }
-            if (board[coor_height+1][coor_width] != '-')// 1x0
+            if (board[coor_height][coor_width+1] != '-' &&
+                (board[coor_height-1][coor_width] != '-' || board[coor_height-1][coor_width+1] != '-' || board[coor_height-1][coor_width+2] != '-'))// 0x1
             {
-                if ( board[coor_height+2][coor_width-1] != '-' || board[coor_height+1][coor_width-1] != '-' || board[coor_height][coor_width-1] != '-')
-                {
-                    check_value = 1;
-                }
+                         return 1;
             }
-            if (board[coor_height+2][coor_width] != '-')// 2x0
+            if (board[coor_height][coor_width+2] != '-' &&
+                (board[coor_height-1][coor_width+1] != '-' || board[coor_height-1][coor_width+2] != '-'))// 0x2
             {
-                if ( board[coor_height+2][coor_width-1] != '-' || board[coor_height+1][coor_width-1] != '-')
-                {
-                    check_value = 1;
-                }
+                         return 1;
             }
-
-         }
-         if (coor_width == 0 && coor_width != 0)
+            return 0;
+         }else
          {
-            if (board[coor_height][coor_width] != '-')// 0x0
-            {
-                if ( board[coor_height-1][coor_width] != '-' || board[coor_height-1][coor_width+1] != '-')
-                {
-                    check_value = 1;
-                }
-            }
-            if (board[coor_height][coor_width+1] != '-')// 0x1
-            {
-                if ( board[coor_height-1][coor_width] != '-' || board[coor_height-1][coor_width+1] != '-' || board[coor_height-1][coor_width+2] != '-')
-                {
-                    check_value = 1;
-                }
-            }
-            if (board[coor_height][coor_width+2] != '-')// 0x2
-            {
-                if ( board[coor_height-1][coor_width+1] != '-' || board[coor_height-1][coor_width+2] != '-')
-                {
-                    check_value = 1;
-                }
-            }
-
+             return 0;
          }
 
-     }
-    return check_value;
  }
 
