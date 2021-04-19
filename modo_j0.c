79ca5b855edb8a2bd6c6e4b3106aeb9aa0f24484
Diogo Valdrez <diogovaldrez@tecnico.ulisboa.pt>
2021-04-19 11:25:31 +0100

    Added modo_p2 on modo_j0

--- modo_j0.c
+++ modo_j0.c
@@ -5,7 +5,7 @@ int j0(char board[15][24], int height, int width, char sqr[3][3], int pie_types[
     if(modo_posicion==1){
         p1(board, height, width, sqr, pie_types);
     }else if (modo_posicion==2){
-        printf("modo posicionamento 2");
+        p2(board, height, width, sqr, pie_types);
     }else{
         return EXIT_FAILURE;
     }  
--- modo_j0.h
+++ modo_j0.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "modo_p1.h"
+#include "modo_p2.h"
 
 int j0(char board[15][24], int height, int width, char sqr[3][3], int pie_types[8], int modo_posicion);
 #endif
