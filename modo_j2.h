84f225ee1e1dc3f9e5538a66d0f240a11d300ab5
Diogo Valdrez <diogovaldrez@tecnico.ulisboa.pt>
2021-04-20 16:36:22 +0100

    Added modo_j2

---
+++ modo_j2.h
@@ -0,0 +1,10 @@
+#ifndef MODO_J2_HPP
+#define MODO_J2_HPP
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "modo_d1.h"
+#include "board.h"
+
+int mj2(char board[15][24], int height, int width, int pie_types[8], int modo_shot);
+#endif
